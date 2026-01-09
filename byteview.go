06f871c6bbd5157cf1e231b7a0a232619fd5850f
Lenovo <iop929787459@163.com>
2026-01-09 20:46:51 +0800

    feat:byteview&cache

---
+++ byteview.go
@@ -0,0 +1,29 @@
+
+package likecache
+
+// 实现一个只读数据结构ByteView用来表示缓存值，对于一些引用类型的变量，比如切片，返回引用作为值，如果修改了，那么缓存内的值也被修改了
+// 因此实现一个只读数据结构来提供一个拷贝，选择[]byte是为了能够支持任意的数据类型的存储，例如字符串、图片等。
+// b即从缓存中存放的真实值，外界存入/读取的都是ByteView
+type ByteView struct {
+	b []byte
+}
+
+// 拷贝缓存值，并返回拷贝
+func cloneBytes(bytes []byte)[]byte{
+	b := make([]byte,len(bytes))
+	copy(b,bytes)
+	return b
+}
+
+// 只返回一个拷贝，避免b被修改
+func(v ByteView)ByteSlice()[]byte{
+	return cloneBytes(v.b)
+}
+
+func(v ByteView)Len()int{
+	return len(v.b)
+}
+
+func(v ByteView)String()string{
+	return string(v.b)
+}
---
+++ cache.go
@@ -0,0 +1,36 @@
+packge likecache
+
+import(
+	"synx"
+	"Cache-Like-GeeCache/lru"
+)
+
+type cache struct {
+	mu         	sync.Mutex
+	lru        	*lru.LRUCache
+	maxBytes 	int64
+}
+
+// sync.Mutex是一个结构体，复制的锁状态和原来不同步，因此只能传引用来控制同一个互斥锁
+func(c *cache)add(key string,value ByteView){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru=lru.New(c.maxBytes,nil)
+	}
+	c.lru.Add(key,value)
+}
+// ByteView的默认零值不是nil，是一个空结构体
+func(c *cache)get(key string)(value ByteView,ok bool){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru!=nil {
+		if value,ok:=c.lru.Get(key);ok{
+			// Get()返回的是lru.Value类型，要作断言
+			return value.(ByteView),ok
+		}
+	}
+	return 
+	
+}
